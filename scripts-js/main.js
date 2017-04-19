359a7624c6ea49ba1593197d2445616ec6a9cb29
Rland12 <rland7596@withs.org>
2017-04-19 22:32:13 +0000

    Saved messages, styled text and images

--- scripts-js/main.js
+++ scripts-js/main.js
@@ -2,43 +2,49 @@ function createPostElement(postId, title, text, author, authorId, authorPic) {
     var uid = firebase.auth().currentUser.uid;
 
     // use jQuery to create a post element with the given values
-    // TODO
-    var postElement = $('<div id="' + postId + '"><div class="title">' + title + '</div><div class="text">' + text + '</div></div>');
-
-    return postElement;
+    
+   //clones post and updates it each time
+    var clone = $("#post-template").clone();
+    clone.find(".title").html(title);
+    clone.find(".text").html(text);
+    clone.find(".authorPic").attr('src', authorPic );
+    clone.find(".author").html(author);
+    clone.attr("id", "post" + postId);
+    clone.show();
+    return clone;
 }
 
 
 // Saves a new post to the Firebase DB.
 function writeNewPost(uid, username, picture, title, body) {
-  // A post entry.
-  var postData = {
-    author: username,
-    uid: uid,
-    body: body,
-    title: title,
-    authorPic: picture
-  };
+    // A post entry.
+    var postData = {
+        author: username,
+        uid: uid,
+        body: body,
+        title: title,
+        authorPic: picture
+    };
 
-  // Get a key for a new Post.
-  var newPostKey = firebase.database().ref().child('posts').push().key;
+    // Get a key for a new Post.
+    var newPostKey = firebase.database().ref().child('posts').push().key;
 
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  var updates = {};
-  updates['/posts/' + newPostKey] = postData;
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    var updates = {};
+    updates['/posts/' + newPostKey] = postData;
 
-  return firebase.database().ref().update(updates);
+    return firebase.database().ref().update(updates);
 }
 
 // Creates a new post for the current user.
 function newPostForCurrentUser(title, text) {
-  var userId = firebase.auth().currentUser.uid;
-  return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
-    var username = snapshot.val().username;
-    return writeNewPost(firebase.auth().currentUser.uid, username,
-        firebase.auth().currentUser.photoURL,
-        title, text);
-  });
+    var userId = firebase.auth().currentUser.uid;
+    return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+        var username = snapshot.val().username;
+        return writeNewPost(firebase.auth().currentUser.uid, username,
+            firebase.auth().currentUser.photoURL,
+            title, text);
+    });
 }
 
 function fetchDataFromFirebase() {
@@ -49,7 +55,7 @@ function fetchDataFromFirebase() {
         var post = createPostElement(data.key, data.val().title, data.val().body, author, data.val().uid, data.val().authorPic);
         $('#recent-posts-list').append(post);
     });
-    recentPosts.on('child_changed', function(data) {   
+    recentPosts.on('child_changed', function(data) {
         // Do nothing for now, but this lets us update the UI if a post is changed
         console.log('child changed', data);
     });
@@ -63,11 +69,12 @@ function storeUserDataInFirebase(userId, name, email, imageUrl) {
     firebase.database().ref('users/' + userId).set({
         username: name,
         email: email,
-        profile_picture : imageUrl
+        profile_picture: imageUrl
     });
 }
 
 var currentUID;
+
 function userSignedInOrOut(user) {
     // the current user didn't actually change
     if (user && currentUID === user.uid) {
@@ -81,7 +88,8 @@ function userSignedInOrOut(user) {
 
         storeUserDataInFirebase(user.uid, user.displayName, user.email, user.photoURL);
         fetchDataFromFirebase();
-    } else {
+    }
+    else {
         currentUID = null;
         $('#signed-out-view').show();
         $('#signed-in-view').hide();
@@ -112,7 +120,7 @@ $(document).ready(function() {
         var text = $('#new-post-message').val();
         var title = $('#new-post-title').val();;
         if (text && title) {
-            newPostForCurrentUser(title, text)
+            newPostForCurrentUser(title, text);
             $('#new-post-message').val('');
             $('#new-post-title').val('');
         }
--- scripts-js/script.js
+++ scripts-js/script.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
 		$("#wall").prepend("<div>" + "<img id='profile' src='http://www.scass.ae/en/About1/PublishingImages/Pages/staff/nophoto.jpg'>" + text + "</div>");
 		$("#wall").css("background-color", "rgba(0, 0, 0, 0.8)");
 		$("#wall").css("font-size", 25);
-		
 		$("#profile").width(90);
 		$("#profile").css("margin-right", 20);
 		$("#profile").css("margin-top", 43);
